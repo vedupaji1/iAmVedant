4b12d84ed4e94898eff7030933caec6d420bb10b
vedupaji1 <85487800+vedupaji1@users.noreply.github.com>
2022-04-10 11:43:40 +0530

    Add files via upload

---
+++ client/src/Components/BackgroundAnimation/BackgroundAnimation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+const BackgroundAnimation = () => {
+    return (
+        <>
+            <ul className="background">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+    )
+}
+
+export default BackgroundAnimation;
